d44bdd29c0a8d4bd0aed793e3e4967dac90ef6de
D3v1C333 <65844132+D3v1C333@users.noreply.github.com>
2021-10-16 13:57:47 +0500

    fix

--- app/src/main/java/com/example/condom/speakers/SpeakersAdapter.java
+++ app/src/main/java/com/example/condom/speakers/SpeakersAdapter.java
@@ -1,15 +1,11 @@
 package com.example.condom.speakers;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
+
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,46 +13,27 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.condom.R;
-import com.example.condom.favorites.FavoritesDB;
-import com.example.condom.favorites.PerformancesCardsItem;
-
 import java.util.ArrayList;
 
 public class SpeakersAdapter extends RecyclerView.Adapter<SpeakersAdapter.ViewHolder> {
     private ArrayList<SpeakersCardsItem> speakersCardsItems;
-    private Context context;
-    private FavoritesDB favoritesDB;
 
     public SpeakersAdapter(ArrayList<SpeakersCardsItem> speakersCardsItems, Context context){
         this.speakersCardsItems = speakersCardsItems;
-        this.context = context;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        favoritesDB = new FavoritesDB(context);
-        SharedPreferences sharedPreferences = context.getSharedPreferences("prefs", Context.MODE_PRIVATE);
-        boolean firstStart = sharedPreferences.getBoolean("firstStart", true);
-        if(firstStart) createTableOnFirstStart();
-
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_speaker_item,
                 parent, false);
         return new ViewHolder(view);
     }
 
-    private void createTableOnFirstStart() {
-        favoritesDB.insertEmpty();
-        SharedPreferences sharedPreferences = context.getSharedPreferences("prefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean("firstStart", false);
-        editor.apply();
-    }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final SpeakersCardsItem speakersCardsItem = speakersCardsItems.get(position);
-
         holder.mName.setText(speakersCardsItem.getSpeakerName());
         holder.mImageView.setImageResource(speakersCardsItem.getSpeakerImage());
         holder.mProfession.setText(speakersCardsItem.getSpeakerProfession());
@@ -80,7 +57,6 @@ public class SpeakersAdapter extends RecyclerView.Adapter<SpeakersAdapter.ViewHo
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
             mProfession = itemView.findViewById(R.id.t_speaker_profession);
             mImageView = itemView.findViewById(R.id.image_speaker);
             mName = itemView.findViewById(R.id.t_speaker_name);
